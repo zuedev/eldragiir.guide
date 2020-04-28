9f0ccee882dd46092b67cf27110ed59a8a38974e
zuedev <zuedev@gmail.com>
2020-04-28 16:06:19 +0100

    fix mobile youtube autoplay

--- docs/_assets/js/youtubeBackground.js
+++ docs/_assets/js/youtubeBackground.js
@@ -1,3 +1,10 @@
 $(() => {
-  $(".video-background").delay(2000).fadeIn(3000);
+  if (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  ) {
+    console.warn("autoplay of videos is not supported on this device");
+  } else {
+    $(".video-background").delay(2000).fadeIn(3000);
+  }
 });
