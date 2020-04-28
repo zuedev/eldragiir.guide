f7942e40fb7249c40e84fb43c48f0187dea51e39
zuedev <zuedev@gmail.com>
2020-04-28 16:13:58 +0100

    another fix for mobile video backgrounds...

--- docs/_assets/js/youtubeBackground.js
+++ docs/_assets/js/youtubeBackground.js
@@ -1,8 +1,5 @@
 $(() => {
-  if (
-    typeof window.orientation !== "undefined" ||
-    navigator.userAgent.indexOf("IEMobile") !== -1
-  ) {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
     console.warn("autoplay of videos is not supported on this device");
   } else {
     $(".video-background").delay(2000).fadeIn(3000);
